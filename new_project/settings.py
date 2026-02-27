188ac6d39e37d49efc61c8eca926638a024260e4
Manqoba Hlongwane <mmanqoba866@gmail.com>
2026-02-27 17:08:55 +0200

    Update Django settings

--- new_project/settings.py
+++ new_project/settings.py
@@ -67,15 +67,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'news_db',
-        'USER': 'news_user',
-        'PASSWORD': 'StrongPassword123',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
